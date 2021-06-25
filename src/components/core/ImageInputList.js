f00c4a9ad251f3e42a618b71ad90a016ca2d6717
DanielSameh <danielsameh35@gmail.com>
2021-06-25 17:15:43 +0200

    fix bugs

--- src/components/core/ImageInputList.js
+++ src/components/core/ImageInputList.js
@@ -1,38 +1,38 @@
-import React, { useRef } from "react";
-import { ScrollView, View } from "react-native";
-import styled from "styled-components";
-import ImageInput from "./ImageInput";
+import React, { useRef } from 'react'
+import { ScrollView, View } from 'react-native'
+import styled from 'styled-components'
+import ImageInput from './ImageInput'
 
 const ImageInputList = ({ imageUris = [], onRemoveImage, onAddImage }) => {
-    const scrollView = useRef();
-    const scrollEnd = () => {
-        scrollView.current.scrollToEnd();
-    };
-    return (
-        <View style={{ justifyContent: 'center', height: 161 }}>
-            <ScrollView
-                ref={scrollView}
-                horizontal
-                onContentSizeChange={scrollEnd}
-                showsVerticalScrollIndicator={false}
-                showsHorizontalScrollIndicator={false}
-            >
-                <Container>
-                    {imageUris.map((uri) => (
-                        <ImageView key={uri} >
-                            <ImageInput
-                                imageUri={uri}
-                                key={uri}
-                                onChangeImage={() => { onRemoveImage(uri) }}
-                            />
-                        </ImageView>
-                    ))}
-                    <ImageInput onChangeImage={(uri) => onAddImage(uri)} />
-                </Container>
-            </ScrollView>
-        </View>
-    );
-};
+  const scrollView = useRef()
+  const scrollEnd = () => {
+    scrollView.current.scrollToEnd()
+  }
+  return (
+    <View style={{ justifyContent: 'center', height: 161 }}>
+      <ScrollView
+        ref={scrollView}
+        horizontal
+        onContentSizeChange={scrollEnd}
+        showsVerticalScrollIndicator={false}
+        showsHorizontalScrollIndicator={false}
+      >
+        <Container>
+          {imageUris.map((uri) => (
+            <ImageView key={uri} >
+              <ImageInput
+                imageUri={uri}
+                key={uri}
+                onChangeImage={() => { onRemoveImage(uri) }}
+              />
+            </ImageView>
+          ))}
+          <ImageInput onChangeImage={(uri) => onAddImage(uri)} />
+        </Container>
+      </ScrollView>
+    </View>
+  )
+}
 
 const Container = styled(View)`
 flex-direction: row;
@@ -47,4 +47,4 @@ margin-right: 5px;
 margin-left: 5px;
 `
 
-export default ImageInputList;
+export default ImageInputList
